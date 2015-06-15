6235df456b2e57012f257d54913d2c6298dc53ba
imorti <imortimer@pivotal.io>
2015-06-15 17:02:12 -0400

    now checks for 200 HTTP response to make sure Ops Manager is up and running.

--- test_200.sh
+++ test_200.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-response=$(curl -sL -w "%{http_code} %{url_effective}\\n" "http://localhost:3000" -o /dev/null)
+response=$(curl -sL -w "%{http_code}" "http://localhost:3000" -o /dev/null)
 
-response=${response// /}
+# response=${response// /}
 
-echo $response 
+# echo $response 
 
-if [ $response = 200http://localhost:3000/ ]; then 
+if [ $response = 200 ]; then 
 	echo server is up and running
 else
 	echo server is not running
