64b33ee5d44185685fdc8652c61df97e0109187c
imorti <imortimer@pivotal.io>
2015-06-16 09:53:17 -0400

    added HTTP 200 code check to see if Ops Manager is running.

--- test_200.sh
+++ test_200.sh
@@ -11,3 +11,11 @@ else
 	echo server is not running
 fi
 	
+
+
+#!/bin/bash
+until [ 200 = $(curl -sL -w "%{http_code}" "http://localhost:3000" -o /dev/null)]; do
+
+	$(curl -sL -w "%{http_code}" "http://localhost:3000" -o /dev/null
+		
+done
